936153f0f69e8def01eecf400290a3ccc911356f
haiafc1122 <mr.haicaca@gmail.com>
2015-11-16 22:32:01 +0700

    tuan 8

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -11,19 +11,4 @@ class StaticPagesControllerTest < ActionController::TestCase
     assert_response :success
   end
 
-  test "should get login" do
-    get :login
-    assert_response :success
-  end
-
-  test "should get about" do
-    get :about
-    assert_response :success
-  end
-
-  test "should get contact" do
-    get :contact
-    assert_response :success
-  end
-
 end
